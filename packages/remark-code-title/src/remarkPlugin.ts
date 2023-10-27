05c8c9f32e40139400cd234c4b6ca556144c10f0
sbin0819 <sbinha123@gmail.com>
2023-10-27 12:37:54 +0900

    build: fix

--- packages/remark-code-title/src/remarkPlugin.ts
+++ packages/remark-code-title/src/remarkPlugin.ts
@@ -1,36 +1,36 @@
-import { visit } from "unist-util-visit";
-import type * as mdast from "mdast";
-import type * as unified from "unified";
+import { visit } from 'unist-util-visit'
+import type * as mdast from 'mdast'
+import type * as unified from 'unified'
 
 export const remarkCodeTitle: unified.Plugin<[], mdast.Root> = () => {
   return (tree, file) => {
-    visit(tree, "code", (node, index, parent) => {
-      const metaString = `${node.lang ?? ""} ${node.meta ?? ""}`.trim();
-      if (!metaString) return;
+    visit(tree, 'code', (node, index, parent) => {
+      const metaString = `${node.lang ?? ''} ${node.meta ?? ''}`.trim()
+      if (!metaString) return
       // const [title] = metaString.match(/(?<=title=("|'))(.*?)(?=("|'))/) ?? [""];
-      const match = metaString.match(/title=("|')(.*?)(\1)/);
-      const title = match ? match[2] : "";
-      if (!title && metaString.includes("title=")) {
-        file.message("Invalid title", node, "remark-code-title");
-        return;
+      const match = metaString.match(/title=("|')(.*?)(\1)/)
+      const title = match ? match[2] : ''
+      if (!title && metaString.includes('title=')) {
+        file.message('Invalid title', node, 'remark-code-title')
+        return
       }
-      if (!title) return;
+      if (!title) return
 
       const titleNode: mdast.Paragraph = {
-        type: "paragraph",
+        type: 'paragraph',
         data: {
-          hName: "div",
+          hName: 'div',
           hProperties: {
-            "data-remark-code-title": true,
-            "data-language": node.lang,
+            'data-remark-code-title': true,
+            'data-language': node.lang,
           },
         },
-        children: [{ type: "text", value: title }],
-      };
+        children: [{ type: 'text', value: title }],
+      }
 
-      parent.children.splice(index, 0, titleNode);
+      parent.children.splice(index, 0, titleNode)
       /* Skips this node (title) and the next node (code) */
-      return index + 2;
-    });
-  };
-};
+      return index + 2
+    })
+  }
+}
